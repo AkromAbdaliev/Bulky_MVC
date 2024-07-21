4a2eea2527a397add6e786488780bca87be5a791
Akrom Abdaliev <jacquesalaiyxs@gmail.com>
2024-07-21 17:16:05 +0500

    IRepository and ICategoryRepository interfaces and classes which implments them are added

---
+++ Bulky.DataAccessLibrary/Repository/IRepositroy/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bulky.ModelsLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccessLibrary.Repository.IRepositroy
+{
+	public interface ICategoryRepository: IRepository<Category>
+	{
+		void Update(Category obj);
+		void Save();	
+	}
+}
