2553a142382ce5d654382f8b86c52145d1221566
Akrom Abdaliev <jacquesalaiyxs@gmail.com>
2024-07-24 15:08:03 +0500

    Product has created and added to drop down menu

---
+++ Bulky.DataAccessLibrary/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Bulky.DataAccessLibrary.Repository.IRepositroy;
+using Bulky.ModelsLibrary;
+using BulkyWeb.DataAccessLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccessLibrary.Repository
+{
+	public class ProductRepository : Repository<Product>, IProductRepository
+    {
+		private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+  
+
+		public void Update(Product obj)
+		{
+			_db.Products.Update(obj);
+		}
+	}
+}
